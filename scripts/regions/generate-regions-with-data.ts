0b79f245687788e984463892d159e5d317630bee
Pablo Navarro <pnavarrc@gmail.com>
2023-01-31 14:56:17 -0800

    Remove csv-parse

--- scripts/regions/generate-regions-with-data.ts
+++ scripts/regions/generate-regions-with-data.ts
@@ -1,8 +1,9 @@
-import { parse as csvParseSync } from "csv-parse/sync";
 import fs from "fs";
 // eslint-disable-next-line lodash/import-scope
 import { chain } from "lodash";
 import * as path from "path";
+
+import { parseCsv } from "@actnowcoalition/metrics";
 
 const publicDataDir = path.join(__dirname, "../../public/data");
 
@@ -10,20 +11,16 @@ const inputCsvPath = path.join(publicDataDir, "world-happiness.csv");
 const outputCsvPath = path.join(publicDataDir, "regions-with-data.json");
 
 async function main() {
-  const rows = readCsvFile(inputCsvPath);
+  const csvText = fs.readFileSync(inputCsvPath).toString();
+  const rows = parseCsv(csvText);
   // Generates a list of unique region IDs, eliminating empty values
   const regionIdList = chain(rows)
-    .map((region) => region.regionId)
+    .map((row) => row.regionId)
     .compact()
     .uniq()
     .value();
 
   fs.writeFileSync(outputCsvPath, JSON.stringify(regionIdList, null, 2));
-}
-
-function readCsvFile(csvPath: string) {
-  const csvContent = fs.readFileSync(csvPath).toString();
-  return csvParseSync(csvContent, { columns: true });
 }
 
 main().catch((err) => {
