9e44991106b7a5a671ba77d8343b7d21ef4733fe
Fai Hegberg <fai.hegberg@gmail.com>
2023-02-01 15:11:37 -0800

    Add overall score chart

--- src/screens/Location/Location.tsx
+++ src/screens/Location/Location.tsx
@@ -1,19 +1,10 @@
 import PeopleIcon from "@mui/icons-material/People";
-import {
-  Box,
-  Button,
-  Grid,
-  Paper,
-  Stack,
-  Typography,
-  useTheme,
-} from "@mui/material";
+import { Box, Button, Paper, Stack, Typography, useTheme } from "@mui/material";
 
 import {
   AutoWidth,
   MetricCompareTable,
   MetricLineThresholdChart,
-  MetricOverview,
   MultiRegionMultiMetricChart,
   Region,
 } from "@actnowcoalition/actnow.js";
@@ -47,6 +38,7 @@ export const Location: React.FC<{ region: Region; page: Page }> = ({
         socialImgHeight={metaTags.socialImgHeight}
       />
       <PageContainer maxWidth="md">
+        {/* Above the fold */}
         <PageSection>
           <Stack
             direction={{ xs: "column", sm: "row" }}
@@ -81,6 +73,7 @@ export const Location: React.FC<{ region: Region; page: Page }> = ({
         <BorderedPageSection sx={{ backgroundColor: "white" }}>
           <LocationOverview region={region} />
         </BorderedPageSection>
+        {/* Below the fold */}
         <PageSection
           sx={{
             backgroundColor: theme.palette.common.white,
@@ -88,58 +81,71 @@ export const Location: React.FC<{ region: Region; page: Page }> = ({
             borderRadius: 3,
           }}
         >
-          <Stack spacing={3}>
-            <Typography variant="h2">Compare</Typography>
-            <Paper style={{ height: 500, overflow: "auto" }}>
-              <MetricCompareTable
-                regionDB={regions}
-                regions={regions.all}
-                metrics={ALL_METRICS}
-              />
-            </Paper>
-            <Box display="flex" justifyContent={{ sm: "flex-end" }}>
-              <Button variant="outlined" size="large" endIcon={<PeopleIcon />}>
-                Share
-              </Button>
-            </Box>
-          </Stack>
-        </PageSection>
-        <BorderedPageSection sx={{ backgroundColor: "white" }}>
-          {/* Example of a responsive layout */}
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={3}>
-              <MetricOverview region={region} metric={MetricId.LIFE_LADDER} />
-            </Grid>
-            <Grid item xs={12} md>
+          <Stack spacing={10}>
+            {/* Compare Table */}
+            <Stack spacing={3}>
+              <Typography variant="h2">Compare</Typography>
+              <Paper style={{ height: 500, overflow: "auto" }}>
+                <MetricCompareTable
+                  regionDB={regions}
+                  regions={regions.all}
+                  metrics={ALL_METRICS}
+                />
+              </Paper>
+              <Box display="flex" justifyContent={{ sm: "flex-end" }}>
+                <Button
+                  variant="outlined"
+                  size="large"
+                  endIcon={<PeopleIcon />}
+                >
+                  Share
+                </Button>
+              </Box>
+            </Stack>
+            {/* Overall Score Chart */}
+            <Stack spacing={3}>
+              <Typography variant="h2">Overall Score</Typography>
               <AutoWidth>
                 <MetricLineThresholdChart
                   region={region}
                   metric={MetricId.LIFE_LADDER}
-                  height={250}
+                  height={400}
                   width={0}
                 />
               </AutoWidth>
-            </Grid>
-          </Grid>
-        </BorderedPageSection>
-        <PageSection>
-          <Stack spacing={2}>
-            <Typography variant="h2">Explore</Typography>
-            <AutoWidth>
-              <MultiRegionMultiMetricChart
-                metrics={ALL_METRICS}
-                regions={regions.all}
-                initialMetric={MetricId.LIFE_LADDER}
-                initialRegions={[
-                  region,
-                  regions.findByRegionIdStrict("LBN"),
-                  regions.findByRegionIdStrict("NER"),
-                  regions.findByRegionIdStrict("FIN"),
-                ]}
-                height={440}
-                width={0}
-              />
-            </AutoWidth>
+              <Typography variant="paragraphLarge">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                sit amet imperdiet lectus.
+              </Typography>
+              <Box display="flex" justifyContent={{ sm: "flex-end" }}>
+                <Button
+                  variant="outlined"
+                  size="large"
+                  endIcon={<PeopleIcon />}
+                >
+                  Share
+                </Button>
+              </Box>
+            </Stack>
+            {/* Explore Chart */}
+            <Stack spacing={2}>
+              <Typography variant="h2">Explore</Typography>
+              <AutoWidth>
+                <MultiRegionMultiMetricChart
+                  metrics={ALL_METRICS}
+                  regions={regions.all}
+                  initialMetric={MetricId.LIFE_LADDER}
+                  initialRegions={[
+                    region,
+                    regions.findByRegionIdStrict("LBN"),
+                    regions.findByRegionIdStrict("NER"),
+                    regions.findByRegionIdStrict("FIN"),
+                  ]}
+                  height={440}
+                  width={0}
+                />
+              </AutoWidth>
+            </Stack>
           </Stack>
         </PageSection>
       </PageContainer>
